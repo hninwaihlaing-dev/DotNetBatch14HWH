8f826656a32de025a9fb52395620bfafd15c9dda
USER <USER@LAPTOP-JLC9N4P3>
2024-11-16 10:43:02 +0630

    Add project files.

---
+++ DotNetBatch14HWH.ConsoleApp/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+Console.WriteLine("Enter User Name");
+string UserName = Console.ReadLine();
+
+Console.WriteLine("Hello " +  UserName); //one ways
+Console.WriteLine($"Hello {UserName}"); //two ways
+Console.ReadLine();
+
+//Blog
+
+//Id
+//Title
+//Author
+//Content
