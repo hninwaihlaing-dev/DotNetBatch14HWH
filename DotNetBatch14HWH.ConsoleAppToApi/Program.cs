3764515219330200b5a5a5fe474e9d2e7acc0431
Hnin Wai Hlaing <hninwaihlaing.dev@gmail.com>
2024-12-20 16:26:49 +0630

    Feature : Console Request to Api

---
+++ DotNetBatch14HWH.ConsoleAppToApi/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+app.MapGet("/api/example", () => "Helllo, World!");
+app.Run();
